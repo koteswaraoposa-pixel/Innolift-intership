9a45f5ca6150c29b4b9b2f21461bd08e5e0ed6db
P koteswara rao <koteswaraoposa@gmail.com>
2026-06-02 12:11:19 +0530

    Create Day - 2 folder with 4 Python files

---
+++ Day - 2/grade calculator.py
@@ -0,0 +1,17 @@
+def get_grade(marks):
+    if marks >= 90:
+        return "A"
+    elif marks >= 75:
+        return "B"
+    elif marks >= 60:
+        return "C"
+    elif marks >= 45:
+        return "D"
+    else:
+        return "F"
+
+# Call the function for 5 students
+for i in range(1, 6):
+    marks = int(input(f"Enter marks for Student {i}: "))
+    grade = get_grade(marks)
+    print(f"Student {i} Grade: {grade}")
---
+++ Day - 2/multiplication table generator.py
@@ -0,0 +1,12 @@
+# Function to print multiplication table
+
+def times_table(n):
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
+
+# Calling the function for 3 different numbers
+
+
+
+print("\nTable of 5")
+times_table(5)
