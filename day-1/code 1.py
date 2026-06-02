4b383aa596797c6daff6e390fa997c09a3a5b3cd
P koteswara rao <koteswaraoposa@gmail.com>
2026-06-02 12:00:20 +0530

    Add personal info script to day-1 folder

---
+++ day-1/code 1.py
@@ -0,0 +1,10 @@
+
+# personal_info.py
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+college = input("Enter your college: ")
+branch = input("Enter your branch: ")
+cgpa = float(input("Enter your CGPA: "))
+
+print(f"My name is {name}. I am {age} studying {branch} at {college} with {cgpa} CGPA")
