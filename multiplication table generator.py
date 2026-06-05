99a1f570ade1741b961c8e0f7d0b47c09f531b58
P koteswara rao <koteswaraoposa@gmail.com>
2026-06-05 10:28:03 +0530

    Delete multiplication table generator.py

--- multiplication table generator.py
+++
@@ -1,9 +0,0 @@
-def times_table(n):
-    print(f"\nMultiplication Table for {n}")
-    for i in range(1, 11):
-        print(f"{n} x {i} = {n*i}")
-
-# Call for 3 different numbers
-times_table(5)
-times_table(7)
-times_table(10)
